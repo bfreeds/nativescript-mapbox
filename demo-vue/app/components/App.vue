e07405e52bcfac01e8bdc8c6b2b75964c0d42c1b
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-10-28 14:37:08 -0400

    Render a basic Mapbox map

--- demo-vue/app/components/App.vue
+++ demo-vue/app/components/App.vue
@@ -1,17 +1,36 @@
 <template>
     <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <ActionBar title="NativeScript-Vue ❤️ Mapbox"/>
         <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
+          <Mapbox
+            :accessToken="accessToken"
+            :mapStyle="mapStyle"
+            @mapReady="onMapReady"
+          ></Mapbox>
         </GridLayout>
     </Page>
 </template>
 
 <script >
+  import * as Mapbox from 'nativescript-mapbox'
+
   export default {
     data() {
       return {
-        msg: 'Hello World!'
+        accessToken: null, // TODO: ADD YOUR MAPBOX API TOKEN HERE
+        mapStyle: "light",
+      }
+    },
+    created() {
+      // throw an error if Mapbox Access Token is not set
+      if (!this.accessToken) {
+        throw new Error('Your Mapbox accessToken is not set.  Add your token to the App.vue component data.')
+      }
+    },
+    methods: {
+      onMapReady(event) {
+        console.log('Map Ready!')
+        console.log('mapReady event: ', event) // log to console to inspect
       }
     }
   }
